8b5c30a07075a6d4f7b0d63c81df228fc6663528
p-nandi <priyankar.143@gmail.com>
2014-11-16 19:45:08 -0400

    Common Constants

---
+++ src/org/zeppelin/p3/common/CommonConstants.java
@@ -0,0 +1,33 @@
+package org.zeppelin.p3.common;
+
+import java.util.regex.Pattern;
+
+public class CommonConstants {
+	public static final String WHITESPACE = " ";
+	
+	public static final String COLON = ":";
+	
+	public static final String DOUBLE_QUOTES = "\"";
+	
+	public static final String FIRST_BRACKET_OPEN = "(";
+	
+	public static final String FIRST_BRACKET_CLOSE= ")";
+	
+	public static final String OPERATOR_AND= "AND";
+	
+	public static final String OPERATOR_OR= "OR";
+	
+	public static final String OPERATOR_NOT= "NOT";
+	
+	public static final String SECOND_BRACKET_OPEN= "{";
+	
+	public static final String SECOND_BRACKET_CLOSE= "}";
+	
+	public static final String COMMA= ",";
+	
+	public static final String HASH= "#";
+	
+	public static final String EQUAL_SIGN = "=";
+	
+
+}
