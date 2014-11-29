7a6c00ec533c51933d2384540e9e4f85b18ec974
p-nandi <priyankar.143@gmail.com>
2014-11-28 21:43:32 -0400

    ParserException

---
+++ src/org/zeppelin/p3/parser/ParserException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.zeppelin.p3.parser;
+
+/**
+ * @author pnandi
+ * Generic wrapper exception class for parsing exceptions
+ */
+public class ParserException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4691717901217832517L;
+
+}
