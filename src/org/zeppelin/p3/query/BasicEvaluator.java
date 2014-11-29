981c2bbb8aaf81679cc93dc82d3ad108e36c8554
animesh-kumar <animesh143@gmail.com>
2014-11-29 06:27:35 -0500

    Removal of Hardcoding for userId from BasicEvaluator

--- src/org/zeppelin/p3/query/BasicEvaluator.java
+++ src/org/zeppelin/p3/query/BasicEvaluator.java
@@ -47,8 +47,9 @@ public class BasicEvaluator extends HttpServlet {
 		// q="title:sachin";
 		// bq = "category:sports";
 
-		// Retrieve preferred categories for the given user id
-		int userId = 4;
+		// Fetch the Logged In userId from the session
+		Integer userId = (Integer) request.getSession().getAttribute(
+				"loggedInUserId");
 		ArrayList<String> preferredCategories = new ArrayList<String>();
 		MySQLAccess dao = new MySQLAccess();
 		try {
@@ -69,10 +70,10 @@ public class BasicEvaluator extends HttpServlet {
 				.toArray(new String[preferredCategories.size()]));
 		// }
 		// parameters.set("bq", bq);
-		
-		//parameters = morelikethisQuery(q);
-		
-		//spellcheck(q);
+
+		// parameters = morelikethisQuery(q);
+
+		// spellcheck(q);
 
 		try {
 			QueryResponse q_response = solrServer.query(parameters);
@@ -84,33 +85,39 @@ public class BasicEvaluator extends HttpServlet {
 				obj.put("resultCount", list.size());
 				for (int i = 0; i < list.size(); i++) {
 					QueryResult result = new QueryResult();
-					if(list.get(i).getFieldValue("title")!=null){
-						result.setTitle(list.get(i).getFieldValue("title").toString());
+					if (list.get(i).getFieldValue("title") != null) {
+						result.setTitle(list.get(i).getFieldValue("title")
+								.toString());
 					}
-					if(list.get(i).getFieldValue("content")!=null){
-						result.setContent(list.get(i).getFieldValue("content").toString());
+					if (list.get(i).getFieldValue("content") != null) {
+						result.setContent(list.get(i).getFieldValue("content")
+								.toString());
 					}
-					if(list.get(i).getFieldValue("category")!=null){
-						result.setCategory(list.get(i).getFieldValue("category").toString());
+					if (list.get(i).getFieldValue("category") != null) {
+						result.setCategory(list.get(i)
+								.getFieldValue("category").toString());
 					}
-					if(list.get(i).getFieldValue("source")!=null){
-						result.setSource(list.get(i).getFieldValue("source").toString());
+					if (list.get(i).getFieldValue("source") != null) {
+						result.setSource(list.get(i).getFieldValue("source")
+								.toString());
 					}
-					if(list.get(i).getFieldValue("published_date")!=null){
-						result.setPublishedDate(list.get(i).getFieldValue("published_date").toString());
+					if (list.get(i).getFieldValue("published_date") != null) {
+						result.setPublishedDate(list.get(i)
+								.getFieldValue("published_date").toString());
 					}
-					if(list.get(i).getFieldValue("snippet")!=null){
-						result.setSnippet(list.get(i).getFieldValue("snippet").toString());
+					if (list.get(i).getFieldValue("snippet") != null) {
+						result.setSnippet(list.get(i).getFieldValue("snippet")
+								.toString());
 					}
-					
+
 					jsonResults.add(result);
 				}
 				obj.put("results", jsonResults);
 			}
-			
-			else{
+
+			else {
 				JSONArray spellCorrections = getSpellCorrections(q);
-		        obj.put("spellCorrections", spellCorrections);
+				obj.put("spellCorrections", spellCorrections);
 			}
 			out.println(obj);
 			out.close();
@@ -141,49 +148,50 @@ public class BasicEvaluator extends HttpServlet {
 		String modifiedQuery = q + "OR (category:sports)^2.0";
 		return modifiedQuery;
 	}
-	
-	SolrQuery morelikethisQuery(String q){
-		
-		//http://localhost:8983/solr/select?q=apache&mlt=true&mlt.fl=manu,cat&mlt.mindf=1&mlt.mintf=1&fl=id,score
+
+	SolrQuery morelikethisQuery(String q) {
+
+		// http://localhost:8983/solr/select?q=apache&mlt=true&mlt.fl=manu,cat&mlt.mindf=1&mlt.mintf=1&fl=id,score
 		SolrQuery parameters = new SolrQuery();
 		parameters.set("q", q);
-        parameters.set("defType", "edismax");
-        parameters.set("mlt.fl", "title,content,category");
-        parameters.set("mlt", "true");
-        //parameters.set("mlt.mindf", "1");
-        //parameters.set("mlt.mintf", "1");
+		parameters.set("defType", "edismax");
+		parameters.set("mlt.fl", "title,content,category");
+		parameters.set("mlt", "true");
+		// parameters.set("mlt.mindf", "1");
+		// parameters.set("mlt.mintf", "1");
 		return parameters;
 	}
-	
-	
+
 	/**
 	 * Returns the array of suggested spellings
+	 * 
 	 * @param q
 	 * @return
 	 */
-	JSONArray getSpellCorrections(String q){
+	JSONArray getSpellCorrections(String q) {
 		String urlString = "http://localhost:8080/solr-4.10.2";
 		SolrServer solrServer = new HttpSolrServer(urlString);
-		//http://localhost:8080/solr-4.10.2/suggest?q=ac
-		//http://localhost:8080/solr-4.10.2/spell?q=delll%20ultrashar&spellcheck=true&spellcheck.collate=true&spellcheck.build=true
+		// http://localhost:8080/solr-4.10.2/suggest?q=ac
+		// http://localhost:8080/solr-4.10.2/spell?q=delll%20ultrashar&spellcheck=true&spellcheck.collate=true&spellcheck.build=true
 		SolrQuery parameters = new SolrQuery();
 		parameters.setRequestHandler("/spell");
 		parameters.set("q", q);
 		parameters.set("defType", "edismax");
-        parameters.set("spellcheck", "true");
-        parameters.set("spellcheck.collate", "true");
-        parameters.set("spellcheck.build", "true");
+		parameters.set("spellcheck", "true");
+		parameters.set("spellcheck.collate", "true");
+		parameters.set("spellcheck.build", "true");
 		QueryResponse q_response;
 		JSONArray spellCorrections = new JSONArray();
 		try {
 			q_response = solrServer.query(parameters);
-			SpellCheckResponse spellCheckResp = q_response.getSpellCheckResponse();
-			List<Suggestion> list=spellCheckResp.getSuggestions();
-			if(!list.isEmpty()){
-				Suggestion suggestion=list.get(0);
-				List alternatives=suggestion.getAlternatives();
+			SpellCheckResponse spellCheckResp = q_response
+					.getSpellCheckResponse();
+			List<Suggestion> list = spellCheckResp.getSuggestions();
+			if (!list.isEmpty()) {
+				Suggestion suggestion = list.get(0);
+				List alternatives = suggestion.getAlternatives();
 				if (!alternatives.isEmpty()) {
-					for(Object alternative:alternatives){
+					for (Object alternative : alternatives) {
 						SpellCorrection spellCorrection = new SpellCorrection();
 						spellCorrection.setCorrection(alternative.toString());
 						spellCorrections.add(spellCorrection);
