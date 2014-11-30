5f33b292928aabf05528d7ba151dbfacf09d1025
Aniket Deole <aniket.deole@gmail.com>
2014-11-30 05:56:01 -0500

    Fixed bug in sending User Categories to servlet on update.

--- src/org/zeppelin/p3/personalization/UpdateCategoryLikeScore.java
+++ src/org/zeppelin/p3/personalization/UpdateCategoryLikeScore.java
@@ -33,8 +33,8 @@ public class UpdateCategoryLikeScore extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// String categoriesAndLikeScoreMap =
-		// request.getHeader("categoriesAndLikeScoreMap");
+		 String categoriesAndLikeScoreMapS =
+		 request.getHeader("categoriesAndLikeScoreMap");
 		MySQLAccess dao = new MySQLAccess();
 		// Fetch the Logged In userId from the session
 		Integer loggedInUserId = (Integer) request.getSession().getAttribute(
