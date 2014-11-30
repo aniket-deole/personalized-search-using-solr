7ebdeff87885186b743e8c69582a572dbe4e6036
animesh-kumar <animesh143@gmail.com>
2014-11-30 09:09:51 -0500

    Fix for Leading and Trailing Brackets in All fields

--- src/org/zeppelin/p3/query/QueryResult.java
+++ src/org/zeppelin/p3/query/QueryResult.java
@@ -4,7 +4,7 @@ import org.json.simple.JSONAware;
 import org.json.simple.JSONObject;
 
 public class QueryResult implements JSONAware {
-	
+
 	private String id;
 	private String title;
 	private String content;
@@ -13,10 +13,11 @@ public class QueryResult implements JSONAware {
 	private String category;
 	private String source;
 	private Integer popularityScore;
-	
-	public QueryResult(){
-		
+
+	public QueryResult() {
+
 	}
+
 	/**
 	 * @return the id
 	 */
@@ -25,7 +26,8 @@ public class QueryResult implements JSONAware {
 	}
 
 	/**
-	 * @param id the id to set
+	 * @param id
+	 *            the id to set
 	 */
 	public void setId(String id) {
 		this.id = id;
@@ -39,9 +41,14 @@ public class QueryResult implements JSONAware {
 	}
 
 	/**
-	 * @param title the title to set
+	 * @param title
+	 *            the title to set
 	 */
 	public void setTitle(String title) {
+		if (title != null && !title.isEmpty() && title.startsWith("[")
+				&& title.endsWith("]")) {
+			title = title.substring(1, title.length() - 1);
+		}
 		this.title = title;
 	}
 
@@ -53,9 +60,14 @@ public class QueryResult implements JSONAware {
 	}
 
 	/**
-	 * @param content the content to set
+	 * @param content
+	 *            the content to set
 	 */
 	public void setContent(String content) {
+		if (content != null && !content.isEmpty() && content.startsWith("[")
+				&& content.endsWith("]")) {
+			content = content.substring(1, content.length() - 1);
+		}
 		this.content = content;
 	}
 
@@ -67,9 +79,14 @@ public class QueryResult implements JSONAware {
 	}
 
 	/**
-	 * @param snippet the snippet to set
+	 * @param snippet
+	 *            the snippet to set
 	 */
 	public void setSnippet(String snippet) {
+		if (snippet != null && !snippet.isEmpty() && snippet.startsWith("[")
+				&& snippet.endsWith("]")) {
+			snippet = snippet.substring(1, snippet.length() - 1);
+		}
 		this.snippet = snippet;
 	}
 
@@ -81,7 +98,8 @@ public class QueryResult implements JSONAware {
 	}
 
 	/**
-	 * @param rating the rating to set
+	 * @param rating
+	 *            the rating to set
 	 */
 	public void setRating(Integer rating) {
 		this.rating = rating;
@@ -95,9 +113,14 @@ public class QueryResult implements JSONAware {
 	}
 
 	/**
-	 * @param category the category to set
+	 * @param category
+	 *            the category to set
 	 */
 	public void setCategory(String category) {
+		if (category != null && !category.isEmpty() && category.startsWith("[")
+				&& category.endsWith("]")) {
+			category = category.substring(1, category.length() - 1);
+		}
 		this.category = category;
 	}
 
@@ -109,9 +132,14 @@ public class QueryResult implements JSONAware {
 	}
 
 	/**
-	 * @param source the source to set
+	 * @param source
+	 *            the source to set
 	 */
 	public void setSource(String source) {
+		if (source != null && !source.isEmpty() && source.startsWith("[")
+				&& source.endsWith("]")) {
+			source = source.substring(1, source.length() - 1);
+		}
 		this.source = source;
 	}
 
@@ -123,25 +151,19 @@ public class QueryResult implements JSONAware {
 	}
 
 	/**
-	 * @param publishedDate the publishedDate to set
+	 * @param publishedDate
+	 *            the publishedDate to set
 	 */
 	public void setPublishedDate(String publishedDate) {
+		if (publishedDate != null && !publishedDate.isEmpty()
+				&& publishedDate.startsWith("[") && publishedDate.endsWith("]")) {
+			publishedDate = publishedDate.substring(1,
+					publishedDate.length() - 1);
+		}
 		this.publishedDate = publishedDate;
 	}
 
 	private String publishedDate;
-
-	public QueryResult(String id, String title, String content, String snippet,
-			Integer rating, String category, String source, String publishedDate) {
-		this.id = id;
-		this.title = title;
-		this.content = content;
-		this.snippet = snippet;
-		this.rating = rating;
-		this.category = category;
-		this.source = source;
-		this.publishedDate = publishedDate;
-	}
 
 	/**
 	 * @return the popularityScore
@@ -149,23 +171,26 @@ public class QueryResult implements JSONAware {
 	public Integer getPopularityScore() {
 		return popularityScore;
 	}
+
 	/**
-	 * @param popularityScore the popularityScore to set
+	 * @param popularityScore
+	 *            the popularityScore to set
 	 */
 	public void setPopularityScore(Integer popularityScore) {
 		this.popularityScore = popularityScore;
 	}
+
 	@Override
 	public String toJSONString() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("{");
-		
+
 		sb.append("\"" + JSONObject.escape("id") + "\"");
 		sb.append(":");
 		sb.append("\"" + JSONObject.escape(id) + "\"");
 
 		sb.append(",");
-		
+
 		sb.append("\"" + JSONObject.escape("title") + "\"");
 		sb.append(":");
 		sb.append("\"" + JSONObject.escape(title) + "\"");
@@ -205,7 +230,7 @@ public class QueryResult implements JSONAware {
 		sb.append("\"" + JSONObject.escape("publishedDate") + "\"");
 		sb.append(":");
 		sb.append("\"" + JSONObject.escape(publishedDate) + "\"");
-		
+
 		sb.append(",");
 
 		sb.append("\"" + JSONObject.escape("popularityScore") + "\"");
